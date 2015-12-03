d77da408406fd7fec90c44cf6c992f638355d1c1
ImJessW <ImJessW@gmail.com>
2015-12-03 01:08:07 -0500

    Creating my first react web app

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports ={
+  entry:"./app/components/Main.js",
+  output:{
+    filename:"public/bundle.js"
+  },
+
+  // This is where we tell it what to do with teh main.js code
+  module:{
+    // Specifying the loader, in this case it's the babel loader
+    loaders:[
+      {
+        test:/\.jsx?$/,
+        exclude: /(node_modules| bower_components)/,
+        loader:'babel'
+      }
+
+    ]
+  }
+};
