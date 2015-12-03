6d6b91e49a2480bd888514b90a410010734d0b4d
ImJessW <ImJessW@gmail.com>
2015-12-03 03:00:34 -0500

    trying to debug the transpolar not working

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,4 +1,5 @@
 var React = require('react');
+console.log (" react is alive")
 // React.createClass creates the component for us. You have a few properties you can pass in, we are using render
 var Main = React.createClass({
   // render specifies what the UI looks like for this specific element
