85e6c2a57ad5994c92046d89b41afced90c9c9e9
ImJessW <ImJessW@gmail.com>
2015-12-14 14:52:08 -0500

    added profile js

---
+++ app/components/profile.js
@@ -0,0 +1,20 @@
+var React= require('react');
+var Router= require("react-router");
+
+var Profile= React.createClass({
+	render; function(){
+		<div className="row">
+			<div className="col-md-4">
+				User Profile Component
+			</div>
+			<div className="col-md-4">
+				Repos Component
+			</div>
+			<div className="col-md-4">
+				Notes Component
+			</div>
+		</div>
+	}
+});
+
+module.exports= profile;
