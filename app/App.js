2a5080867eca2412caf0bd1e85a52b73629f878e
ImJessW <ImJessW@gmail.com>
2016-01-03 15:33:07 -0500

    old dependency removal

--- app/App.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var Router = require('react-router');
-var routes = require('./config/routes');
-
-
-Router.run(routes,function(Root){
-  React.render(<Root />, document.getElementById('app'));
-
-});
