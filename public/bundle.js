1f3cf12353e4d769013023ea3d2e95e3f9147ee5
ImJessW <ImJessW@gmail.com>
2016-01-04 14:25:45 -0500

    updated user profile to add bio and only add is its truethy

--- public/bundle.js
+++ public/bundle.js
@@ -24678,12 +24678,12 @@
 /* 216 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(1);
 
 	var UserProfile = React.createClass({
-		displayName: 'UserProfile',
+		displayName: "UserProfile",
 
 		propTypes: {
 			username: React.PropTypes.string.isRequired,
@@ -24691,20 +24691,74 @@
 
 		},
 		render: function render() {
-			console.log('Bio', this.props.bio);
+
 			return React.createElement(
-				'div',
+				"div",
 				null,
-				React.createElement(
-					'p',
-					null,
-					'User Profile!'
+				this.props.bio.avatar_url && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					" ",
+					React.createElement("img", { src: this.props.bio.avatar_url, className: "img-rounded img-responsive" })
 				),
-				React.createElement(
-					'p',
-					null,
-					'Username: ',
-					this.props.username
+				this.props.bio.name && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Name: ",
+					this.props.bio.name
+				),
+				this.props.bio.login && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Username: ",
+					this.props.bio.login
+				),
+				this.props.bio.email && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Email: ",
+					this.props.bio.email
+				),
+				this.props.bio.location && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Location: ",
+					this.props.bio.location
+				),
+				this.props.bio.company && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Company: ",
+					this.props.bio.company
+				),
+				this.props.bio.followers && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Followers: ",
+					this.props.bio.followers
+				),
+				this.props.bio.following && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Following: ",
+					this.props.bio.following
+				),
+				this.props.bio.following && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Public Repos: ",
+					this.props.bio.public_repos
+				),
+				this.props.bio.blog && React.createElement(
+					"li",
+					{ className: "list-group-item" },
+					"Blog: ",
+					React.createElement(
+						"a",
+						{ href: this.props.bio.blog },
+						" ",
+						this.props.bio.blog
+					)
 				)
 			);
 		}
