a16c36ba951d52349fbd9cae4a57c9d1499439d7
ImJessW <ImJessW@gmail.com>
2016-01-03 21:27:59 -0500

    added react fire

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,8 +3,10 @@ var Router = require('react-router');
 var Repos =require('./Github/Repos');
 var UserProfile =require('./Github/UserProfile');
 var Notes =require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
 
 var Profile = React.createClass({
+	mixins: [ReactFireMixin],
 	getInitialState: function() {
 		return {
 			notes:[1,2,3],
